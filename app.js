135c7c687243db6199141e2ce55185770651c352
vladyslavnUA <vladyslavn@yahoo.com>
2021-11-15 21:35:59 -0500

    search form

--- app.js
+++ app.js
@@ -17,15 +17,8 @@ app.set('view engine', 'handlebars');
 
 // routes
 app.get('/', (req, res) => {
-    // get url of gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
-
-    res.render('hello-gif', { gifUrl })
-});
-
-app.get('/greetings/:name', (req, res) => {
-    const name = req.params.name;
-    res.render('greetings', { name })
+    console.log(req.query)
+    res.render('home');
 });
 
 // begin server
