17c2e77975c28c1ab87db47013aa1408d463a7fe
vladyslavnUA <vladyslavn@yahoo.com>
2021-11-15 21:59:52 -0500

    giffy working

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // require libraries
+const { response } = require('express');
 const express = require('express');
 const exphbs = require('express-handlebars');
+
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "9ATAVVVH21VH", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
 
 // setup app 
 const app = express();
@@ -17,8 +25,19 @@ app.set('view engine', 'handlebars');
 
 // routes
 app.get('/', (req, res) => {
-    console.log(req.query)
-    res.render('home');
+    
+    // home page before query
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+
+    // Tenor.Search.Query("keyword", "limit #")
+    Tenor.Search.Query(term, "10").then(response => {
+        // store gifs
+        const gifs = response;
+        res.render('home', { gifs });
+    }).catch(console.error);    
 });
 
 // begin server
